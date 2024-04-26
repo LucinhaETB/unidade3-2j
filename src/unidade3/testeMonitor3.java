4d176d990d06dcb239039caffe7df4625bd3c23a
LucinhaETB <168144381+LucinhaETB@users.noreply.github.com>
2024-04-25 22:31:43 -0300

    criando o monitor2 e ajustando gitignore

--- src/unidade3/testeMonitor3.java
+++ src/unidade3/testeMonitor3.java
@@ -6,11 +6,13 @@ public class testeMonitor3 {
     public static void main(String[] args) {
         Monitor m = new Monitor();
         Monitor m1 = new Monitor();
+        Monitor m2 = new Monitor();
         m.solicitarInfo();
         m1.solicitarInfo ();
-        
+        m2.solicitarInfo ();
         m.exibirInfo();
         m1.exibirInfo();
+        m2.exibirInfo();
 
     }
 
